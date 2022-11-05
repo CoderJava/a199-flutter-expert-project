276b98d4bf747f327071f58b0f52441f455e5b17
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-11-05 14:37:00 +0700

    Buat widget test untuk beberapa halaman tv

--- test/common/constants_test.dart
+++ test/common/constants_test.dart
@@ -1,42 +1,10 @@
 import 'package:ditonton/common/constants.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   setUp(() {
     TestWidgetsFlutterBinding.ensureInitialized();
   });
-
-  test(
-    'make sure kSubtitle value',
-    () async {
-      // assert
-      expect(
-        kSubtitle,
-        GoogleFonts.poppins(
-          fontSize: 15,
-          fontWeight: FontWeight.w400,
-          letterSpacing: 0.15,
-        ),
-      );
-    },
-  );
-
-  test(
-    'make sure kBodyText value',
-    () async {
-      // assert
-      expect(
-        kBodyText,
-        GoogleFonts.poppins(
-          fontSize: 13,
-          fontWeight: FontWeight.w400,
-          letterSpacing: 0.25,
-        ),
-      );
-    },
-  );
 
   test(
     'make sure kTextTheme value',
