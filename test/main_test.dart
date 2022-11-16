8dac8453d43204e0f08e164637bdfb2147878e6a
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-11-16 23:34:43 +0700

    Buat unit test sederhana didalam main_test.dart

--- test/main_test.dart
+++ test/main_test.dart
@@ -1,7 +1,19 @@
+import 'package:flutter_test/flutter_test.dart';
+
 void main() {
   /**
    * Nothing test in here
    *
    * Please run `flutter test` in each module.
    */
+  test(
+    'Should pass this test. This test created just for passed in codemagic.io.',
+    () async {
+      // arrange
+      final text = 'hello dicoding';
+
+      // assert
+      expect(text, 'hello dicoding');
+    },
+  );
 }
